08f7f3f9a01bbbc8c6b44a1d976f469963f8493d
Jeroen Hofstee <jhofstee@victronenergy.com>
2021-07-25 14:07:26 +0200

    don't throw from a destructor
    Prevents a warning.

--- QsLog.cpp
+++ QsLog.cpp
@@ -250,7 +250,6 @@ Logger::Helper::~Helper()
     catch(std::exception&) {
         // you shouldn't throw exceptions from a sink
         Q_ASSERT(!"exception in logger helper destructor");
-        throw;
     }
 }
 
